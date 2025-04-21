54b6cfbaea5adea4b70fe33227e1b4b9bdce5391
xebec19 <rohant946@gmail.com>
2025-04-21 22:58:15 +0530

    refactor: init auth

---
+++ sample/go.mod
@@ -0,0 +1,3 @@
+module github.com/Xebec19/lms/sample
+
+go 1.22.6
---
+++ sample/internal/server.go
@@ -0,0 +1,29 @@
+package internal
+
+import (
+	"net/http"
+
+	"github.com/Xebec19/lms/common/middlewares"
+	"github.com/Xebec19/lms/common/utils"
+	"github.com/Xebec19/lms/sample/internal/handler"
+	"github.com/gorilla/mux"
+)
+
+func CreateServer() *http.Server {
+	r := mux.NewRouter()
+
+	handleRoutes(r)
+
+	r.Use(middlewares.LoggingMiddleware)
+
+	return &http.Server{
+		Addr:    ":" + utils.GetConfig().Port,
+		Handler: r,
+	}
+}
+
+func handleRoutes(r *mux.Router) {
+	api := r.PathPrefix("/api/v1/sample").Subrouter()
+
+	api.HandleFunc("/health", handler.HandleHealthCheck).Methods("GET")
+}
